c233c2fd1625bff3d9c1be0352eff619a5ee610f
Ignasiya <ignasiya93@gmail.com>
2024-03-05 14:43:00 +0300

    Подготовка к разметки главной страницы

---
+++ src/resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+
+});
+</script>
+
+<template>
+    <Head title="Социальная сеть" />
+
+
+</template>
+
+<style>
+
+</style>
