ce39f178c6322ca71fce578cc4215202319bbb6e
Ignasiya <ignasiya93@gmail.com>
2024-05-02 22:37:58 +0300

    Практика с выносом запроса в сервис слой

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -2,46 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Enums\GroupUserStatus;
 use App\Http\Resources\PostResource;
-use App\Models\Post;
+use App\Service\GetPostsByFollowersAndGroupService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class HomeController extends Controller
 {
-    public function index(Request $request)
+    public function index(GetPostsByFollowersAndGroupService $getPostsByFollowersAndGroup, Request $request): AnonymousResourceCollection|Response
     {
-        $userId = Auth::id();
-
-        $posts = Post::postsForTimeline($userId)
-            ->whereHas('user.followers', function ($query) use ($userId) {
-                $query->where('followers.follower_id', $userId);
-            })
-            ->orWhereHas('group.users', function ($query) use ($userId) {
-                $query->where('group_users.user_id', $userId)
-                    ->where('group_users.status', GroupUserStatus::APPROVED->value);
-            })
-            ->whereNot('posts.user_id', $userId)
-            ->paginate(10);
-
-//        $posts = Post::postsForTimeline($userId)
-//            ->leftJoin('followers AS f', function ($join) use ($userId) {
-//                $join->on('f.user_id', '=', 'posts.user_id')
-//                    ->where('f.follower_id', '=', $userId);
-//            })
-//            ->leftJoin('group_users AS gu', function ($join) use ($userId) {
-//                $join->on('gu.group_id', '=', 'posts.group_id')
-//                    ->where('gu.user_id', '=', $userId)
-//                    ->where('gu.status', GroupUserStatus::APPROVED->value);
-//            })
-//            ->whereNotNull('f.follower_id')
-//            ->orWhereNotNull('gu.group_id')
-//            ->whereNot('posts.user_id', '=', $userId)
-//            ->paginate(10);
+        $posts = $getPostsByFollowersAndGroup(Auth::id());
 
         $posts = PostResource::collection($posts);
+
         if ($request->wantsJson()) {
             return $posts;
         }
---
+++ src/app/Service/GetPostsByFollowersAndGroupService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Service;
+
+use App\Http\Enums\GroupUserStatus;
+use App\Models\Post;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class GetPostsByFollowersAndGroupService
+{
+    /**
+     * Get timeline posts for the given user ID by followers and group.
+     *
+     * @param  int  $userId
+     * @return LengthAwarePaginator
+     */
+    public function __invoke(int $userId): LengthAwarePaginator
+    {
+        $posts = Post::postsForTimeline($userId)
+            ->whereHas('user.followers', function ($query) use ($userId) {
+                $query->where('followers.follower_id', $userId);
+            })
+            ->orWhereHas('group.users', function ($query) use ($userId) {
+                $query->where('group_users.user_id', $userId)
+                    ->where('group_users.status', GroupUserStatus::APPROVED->value);
+            })
+            ->whereNot('posts.user_id', $userId)
+            ->paginate(10);
+
+        return $posts;
+    }
+
+//        $posts = Post::postsForTimeline($userId)
+//            ->leftJoin('followers AS f', function ($join) use ($userId) {
+//                $join->on('f.user_id', '=', 'posts.user_id')
+//                    ->where('f.follower_id', '=', $userId);
+//            })
+//            ->leftJoin('group_users AS gu', function ($join) use ($userId) {
+//                $join->on('gu.group_id', '=', 'posts.group_id')
+//                    ->where('gu.user_id', '=', $userId)
+//                    ->where('gu.status', GroupUserStatus::APPROVED->value);
+//            })
+//            ->whereNotNull('f.follower_id')
+//            ->orWhereNotNull('gu.group_id')
+//            ->whereNot('posts.user_id', '=', $userId)
+//            ->paginate(10);
+
+}
